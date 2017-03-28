6144ea9080a59269cff76f1e67b579cc3cc30742
laggingreflex <laggingreflex@gmail.com>
2017-03-29 03:18:02 +0530

    refactor with async/await for node 7.6

--- index.js
+++ index.js
@@ -95,165 +95,141 @@ module.exports = function koaSSRmiddleware(root, opts) {
     defaultJSDOMResourceLoader(_res || res, _cb || cb)
   });
 
-  return function koaSSR(ctx) {
+  return async function koaSSR(ctx) {
 
     const fullUrl = ctx.protocol + '://' + ctx.host + ctx.originalUrl;
     debug({ fullUrl })
 
-    return checkCache(invokeJSDOM);
 
-    function checkCache(next) {
-      if (opts.cache) {
-        let cache
-        if (typeof opts.cache === 'function') {
-          cache = opts.cache(ctx)
-        } else {
-          cache = opts.cache[ctx.originalUrl]
-        }
-        if (cache) {
-          if (cache.then) {
-            return cache.then(cache => {
-              if (cache) {
-                return opts.render(ctx, cache)
-              } else {
-                return next();
-              }
-            })
-          } else {
-            return opts.render(ctx, cache);
-          }
-        } else {
-          return next();
-        }
+    if (opts.cache) {
+      let cache
+      if (typeof opts.cache === 'function') {
+        cache = opts.cache(ctx)
       } else {
-        return next();
+        cache = opts.cache[ctx.originalUrl]
+      }
+
+      if (cache && cache.then) {
+        cache = await cache;
+      }
+
+      if (cache) {
+        return opts.render(ctx, cache)
       }
     }
 
-    function invokeJSDOM() {
-      debugJSDOM('Loading...')
-      const startTime = new Date();
-      const totalTime = () => (Date.now() - startTime) + 'ms';
+    debugJSDOM('Loading...')
+    const startTime = new Date();
+    const totalTime = () => (Date.now() - startTime) + 'ms';
 
-      const jsdom = JSDOM.jsdom(opts.html, Object.assign({
-        features: {
-          FetchExternalResources: ['script', 'link', 'css'],
-          QuerySelector: true,
-        },
-        resourceLoader: JSDOMResourceLoader,
-        virtualConsole: JSDOMVirtualConsole,
-      }, opts.jsdom, {
-        created: (err, window) => {
-          if (err) {
-            debugJSDOM('[on created] error', err)
-            if (opts.jsdom.created) {
-              opts.jsdom.created(err, window)
-            } else {
-              throw err
-            }
-          }
-          debugJSDOM('[on created] ok')
+    const jsdom = JSDOM.jsdom(opts.html, Object.assign({
+      features: {
+        FetchExternalResources: ['script', 'link', 'css'],
+        QuerySelector: true,
+      },
+      resourceLoader: JSDOMResourceLoader,
+      virtualConsole: JSDOMVirtualConsole,
+    }, opts.jsdom, {
+      created: (err, window) => {
+        if (err) {
+          debugJSDOM('[on created] error', err)
           if (opts.jsdom.created) {
             opts.jsdom.created(err, window)
+          } else {
+            throw err
           }
-        },
-        onload: (window) => {
-          debugJSDOM('[on onload] ok')
-          if (opts.jsdom.onload) {
-            opts.jsdom.onload(window)
-          }
-        },
-        done: (err, window) => {
-          if (err) {
-            debugJSDOM('[on done] error', err)
-            if (opts.jsdom.done) {
-              opts.jsdom.done(err, window)
-            } else {
-              throw err
-            }
-          }
-          debugJSDOM('[on done] ok')
+        }
+        debugJSDOM('[on created] ok')
+        if (opts.jsdom.created) {
+          opts.jsdom.created(err, window)
+        }
+      },
+      onload: (window) => {
+        debugJSDOM('[on onload] ok')
+        if (opts.jsdom.onload) {
+          opts.jsdom.onload(window)
+        }
+      },
+      done: (err, window) => {
+        if (err) {
+          debugJSDOM('[on done] error', err)
           if (opts.jsdom.done) {
             opts.jsdom.done(err, window)
-          }
-        },
-      }));
-      debugJSDOM('loaded')
-
-      const window = jsdom.defaultView;
-
-      JSDOM.changeURL(window, fullUrl);
-      debugJSDOM('URL changed to:', fullUrl)
-
-      let resolve;
-      const loaded = new Promise(r => resolve = r);
-
-      window[opts.modulesLoadedEventLabel] = () => {
-        resolve();
-        loaded.loaded = true;
-      };
-
-      return Promise.race([loaded, delay(opts.timeout)]).then(() => {
-        debugJSDOM(opts.modulesLoadedEventLabel, 'fired in', totalTime());
-        resolve();
-        if (!loaded.loaded) {
-          const err = new Error(`JSDOM Timed out (${parseInt(opts.timeout/1000, 10)}s), \`window.${opts.modulesLoadedEventLabel}\` was never called.`)
-          err.koaSSR = { ctx, window };
-          throw err;
-        }
-
-        // restore modifiedScriptTags (async/defer)
-        for (const script of window.document.querySelectorAll('script')) {
-          const src = URL.parse(script.src).path;
-          if (src in modifiedScriptTags) {
-            script.setAttribute(modifiedScriptTags[src], true);
-          }
-        }
-        debugJSDOM(`restored modifiedScriptTags`);
-
-        const preCache = JSDOM.serializeDocument(window.document);
-        debugJSDOM(`serialized preCache`);
-
-        let final;
-        if (typeof opts.cache === 'function') {
-          final = opts.cache(ctx, preCache, window, JSDOM.serializeDocument);
-          assert(final, `opts.cache() didn't return anything`)
-          if (final.then) {
-            return final.then(final => {
-              assert(final, `opts.cache() didn't resolve to anything`)
-              if (typeof final !== 'string') {
-                try {
-                  final = JSDOM.serializeDocument(final.document);
-                } catch (error) {
-                  error.message = `Failed trying to serialize opts.cache()'s resolved object. ` + error.message;
-                  throw error;
-                }
-              }
-              debugJSDOM(`final opts.render in`, totalTime());
-              return opts.render(ctx, final, window, JSDOM.serializeDocument)
-            })
           } else {
-            if (typeof final !== 'string') {
-              try {
-                final = JSDOM.serializeDocument(final.document);
-              } catch (error) {
-                error.message = `Failed trying to serialize opts.cache()'s returned object. ` + error.message;
-                throw error;
-              }
-            }
-            debugJSDOM(`final opts.render in`, totalTime());
-            return opts.render(ctx, final, window, JSDOM.serializeDocument);
+            throw err
           }
-        } else {
-          final = preCache;
-          if (opts.cache) {
-            opts.cache[ctx.originalUrl] = final;
-          }
-          debugJSDOM(`final opts.render in`, totalTime());
-          return opts.render(ctx, final, window, JSDOM.serializeDocument);
         }
-      });
+        debugJSDOM('[on done] ok')
+        if (opts.jsdom.done) {
+          opts.jsdom.done(err, window)
+        }
+      },
+    }));
+    debugJSDOM('loaded')
+
+    const window = jsdom.defaultView;
+
+    JSDOM.changeURL(window, fullUrl);
+    debugJSDOM('URL changed to:', fullUrl)
+
+    let resolve;
+    const loaded = new Promise(r => resolve = r);
+
+    window[opts.modulesLoadedEventLabel] = () => {
+      resolve();
+      loaded.loaded = true;
+    };
+
+    await Promise.race([loaded, delay(opts.timeout)]);
+
+    debugJSDOM(opts.modulesLoadedEventLabel, 'fired in', totalTime());
+    resolve();
+    if (!loaded.loaded) {
+      const err = new Error(`JSDOM Timed out (${parseInt(opts.timeout/1000, 10)}s), \`window.${opts.modulesLoadedEventLabel}\` was never called.`)
+      err.koaSSR = { ctx, window };
+      throw err;
     }
+
+    // restore modifiedScriptTags (async/defer)
+    for (const script of window.document.querySelectorAll('script')) {
+      const src = URL.parse(script.src).path;
+      if (src in modifiedScriptTags) {
+        script.setAttribute(modifiedScriptTags[src], true);
+      }
+    }
+    debugJSDOM(`restored modifiedScriptTags`);
+
+    const preCache = JSDOM.serializeDocument(window.document);
+    debugJSDOM(`serialized preCache`);
+
+    let final;
+
+    if (typeof opts.cache === 'function') {
+      final = opts.cache(ctx, preCache, window, JSDOM.serializeDocument);
+      assert(final, `opts.cache() didn't return anything`)
+      if (final.then) {
+        final = await final;
+        assert(final, `opts.cache() didn't resolve to anything`)
+      }
+      if (typeof final !== 'string') {
+        try {
+          final = JSDOM.serializeDocument(final.document);
+        } catch (error) {
+          error.message = `Failed trying to serialize opts.cache()'s returned object. ` + error.message;
+          throw error;
+        }
+      }
+    }
+
+    final = final || preCache;
+
+    if (typeof opts.cache === 'object') {
+      opts.cache[ctx.originalUrl] = final;
+      debug(`cached to memory`, ctx.originalUrl, 'total', Object.keys(opts.cache).length);
+    }
+
+    debug(`final opts.render in`, totalTime());
+    return opts.render(ctx, final, window, JSDOM.serializeDocument);
   }
 }
 
